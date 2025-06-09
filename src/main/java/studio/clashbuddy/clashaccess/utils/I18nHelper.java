08d9f07cad4a19049284752356ec1307424d2cf2
Clash Buddy Studio <dev@clashbuddy.studio>
2025-06-09 18:21:08 +0300

    i18n fixing

--- src/main/java/studio/clashbuddy/clashaccess/utils/I18nHelper.java
+++ src/main/java/studio/clashbuddy/clashaccess/utils/I18nHelper.java
@@ -5,6 +5,9 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
 @Component
 public class I18nHelper {
 
@@ -18,21 +21,35 @@ public class I18nHelper {
 
         if (message.startsWith("{") && message.endsWith("}")) {
             var locale = LocaleContextHolder.getLocale();
-            String code = message.substring(1, message.length() - 1);
-            if (messageSource != null) {
-                try {
-                    return messageSource.getMessage(code, args, locale);
-                } catch (NoSuchMessageException e) {
-                    try{
-                         return clashAccessMessageSource.getMessage(code, args, locale);
-                    }catch (NoSuchMessageException es) {
-                        return code;
-                    }
-                }
-            }
-            return code;
+            return processTheMessage(message, locale, args);
         }
 
         return message;
+    }
+
+    public String i18n(String message, Locale locale, Object... args) {
+        if (message == null) return "";
+
+        if (message.startsWith("{") && message.endsWith("}")) {
+            return processTheMessage(message, locale, args);
+        }
+
+        return message;
+    }
+
+    private String processTheMessage(String message, Locale locale, Object[] args) {
+        String code = message.substring(1, message.length() - 1);
+        if (messageSource != null) {
+            try {
+                return messageSource.getMessage(code, args, locale);
+            } catch (NoSuchMessageException e) {
+                try{
+                    return clashAccessMessageSource.getMessage(code, args, locale);
+                }catch (NoSuchMessageException es) {
+                    return code;
+                }
+            }
+        }
+        return code;
     }
 }
